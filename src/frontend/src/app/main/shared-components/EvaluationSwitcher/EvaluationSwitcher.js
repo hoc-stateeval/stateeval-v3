ebc86d9acb58207d4c6bc8e884ad33638e0815aa
Anne Chinn <achinn@nwlink.com>
2021-11-12 13:58:44 -0800

    only show EvaluatorSwitcher when user is evaluator

--- src/frontend/src/app/main/shared-components/EvaluationSwitcher/EvaluationSwitcher.js
+++ src/frontend/src/app/main/shared-components/EvaluationSwitcher/EvaluationSwitcher.js
@@ -6,35 +6,39 @@ import {
   selectEvaluationsAll,
   selectActiveEvaluationId,
   setActiveEvaluationId,
+  selectActiveWorkAreaContext,
 } from '../../../store/stateEval/userContextSlice';
 
 function EvaluationSwitcher() {
   const dispatch = useDispatch();
   const evaluations = useSelector(selectEvaluationsAll);
   const activeEvaluationId = useSelector(selectActiveEvaluationId);
+  const workAreaContext = useSelector(selectActiveWorkAreaContext);
 
   const changeSelectedEvaluation = (e) => {
     const evaluationId = parseInt(e.target.value, 10);
     dispatch(setActiveEvaluationId(evaluationId));
   };
 
-  return (
-    <>
-      <Select
-        value={activeEvaluationId}
-        onChange={changeSelectedEvaluation}
-        displayEmpty
-        name="evaluation"
-        classes={{ select: 'py-8' }}
-      >
-        {evaluations.map((x) => (
-          <MenuItem key={x.id} value={x.id}>
-            {x.evaluateeDisplayName}
-          </MenuItem>
-        ))}
-      </Select>
-    </>
+  const content = workAreaContext?.isEvaluator ? (
+    <Select
+      value={activeEvaluationId}
+      onChange={changeSelectedEvaluation}
+      displayEmpty
+      name="evaluation"
+      classes={{ select: 'py-8' }}
+    >
+      {evaluations.map((x) => (
+        <MenuItem key={x.id} value={x.id}>
+          {x.evaluateeDisplayName}
+        </MenuItem>
+      ))}
+    </Select>
+  ) : (
+    <></>
   );
+
+  return content;
 }
 
 export default EvaluationSwitcher;
