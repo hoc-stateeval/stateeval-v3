10bad63417ea7a39b99e079569cf9e5fb80dee71
Anne Chinn <achinn@nwlink.com>
2021-11-16 09:51:16 -0800

    working on tests

--- src/backend/SE.API.Tests/FrameworkControllerTests.cs
+++ src/backend/SE.API.Tests/FrameworkControllerTests.cs
@@ -157,5 +157,118 @@ namespace SE.API.Tests
             framework.FrameworkTagName.Should().Be("MAR-PR");
         }
 
+        [Fact]
+        public async Task DAN_TR_Eval_State_Framework_Should_Have_Six_FrameworkNodes()
+        {
+            var DAN_District = new District(DistrictNames.DAN, DistrictCodes.DAN);
+            var userName = DAN_District.School1.PrincipalA.UserName;
+
+            var user = await _client.GetAndDeserialize<UserDTO>($"/users/{userName}");
+            var workAreaContexts = await _client.GetAndDeserialize<List<WorkAreaContextDTO>>($"/users/{user.Id}/workarea-contexts");
+
+            workAreaContexts.Count.Should().Be(2);
+            var workAreaContext = workAreaContexts.Find(x => x.TagName == EnumUtils.MapWorkAreaTypeToTagName(WorkAreaType.PR_TR));
+            workAreaContext.Should().NotBeNull();
+
+            var framework = await _client.GetAndDeserialize<FrameworkDTO>($"/frameworks/{workAreaContext.StateFrameworkId}");
+            framework.Should().NotBeNull();
+            framework.Name.Should().Be("Danielson State");
+
+            framework.FrameworkNodes.Count.Should().Be(8);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C1").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C2").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C3").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C4").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C5").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C6").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C7").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "C8").Count().Should().Be(1);
+        }
+
+        [Fact]
+        public async Task DAN_TR_Eval_State_Framework_C1_Should_Have_Three_RubricRows()
+        {
+            var DAN_District = new District(DistrictNames.DAN, DistrictCodes.DAN);
+            var userName = DAN_District.School1.PrincipalA.UserName;
+
+            var user = await _client.GetAndDeserialize<UserDTO>($"/users/{userName}");
+            var workAreaContexts = await _client.GetAndDeserialize<List<WorkAreaContextDTO>>($"/users/{user.Id}/workarea-contexts");
+
+            workAreaContexts.Count.Should().Be(2);
+            var workAreaContext = workAreaContexts.Find(x => x.TagName == EnumUtils.MapWorkAreaTypeToTagName(WorkAreaType.PR_TR));
+            workAreaContext.Should().NotBeNull();
+
+            var framework = await _client.GetAndDeserialize<FrameworkDTO>($"/frameworks/{workAreaContext.StateFrameworkId}");
+            framework.Should().NotBeNull();
+            framework.Name.Should().Be("Danielson State");
+
+            framework.FrameworkNodes.Count.Should().Be(8);
+            var frameworkNode = framework.FrameworkNodes.Where(x => x.ShortName == "C1").FirstOrDefault();
+            var rubricRows = frameworkNode.RubricRows;
+            rubricRows.Count().Should().Be(3);
+            rubricRows.Where(x => x.ShortName == "2b").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "3a").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "3c").Count().Should().Be(1);
+        }
+
+        [Fact]
+        public async Task DAN_TR_Eval_Instructional_Framework_D1_Should_Have_Six_RubricRows()
+        {
+            var DAN_District = new District(DistrictNames.DAN, DistrictCodes.DAN);
+            var userName = DAN_District.School1.PrincipalA.UserName;
+
+            var user = await _client.GetAndDeserialize<UserDTO>($"/users/{userName}");
+            var workAreaContexts = await _client.GetAndDeserialize<List<WorkAreaContextDTO>>($"/users/{user.Id}/workarea-contexts");
+
+            workAreaContexts.Count.Should().Be(2);
+            var workAreaContext = workAreaContexts.Find(x => x.TagName == EnumUtils.MapWorkAreaTypeToTagName(WorkAreaType.PR_TR));
+            workAreaContext.Should().NotBeNull();
+
+            var framework = await _client.GetAndDeserialize<FrameworkDTO>($"/frameworks/{workAreaContext.InstructionalFrameworkId}");
+            framework.Should().NotBeNull();
+            framework.Name.Should().Be("Danielson Instructional");
+
+            framework.FrameworkNodes.Count.Should().Be(4);
+            var frameworkNode = framework.FrameworkNodes.Where(x => x.ShortName == "D1").FirstOrDefault();
+            var rubricRows = frameworkNode.RubricRows;
+            rubricRows.Count().Should().Be(6);
+            rubricRows.Where(x => x.ShortName == "1a").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "1b").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "1c").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "1d").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "1e").Count().Should().Be(1);
+            rubricRows.Where(x => x.ShortName == "1f").Count().Should().Be(1);
+        }
+
+        [Fact]
+        public async Task DAN_TR_Eval_Instructional_Framework_Should_Have_Eight_FrameworkNodes()
+        {
+            var DAN_District = new District(DistrictNames.DAN, DistrictCodes.DAN);
+            var userName = DAN_District.School1.PrincipalA.UserName;
+
+            var user = await _client.GetAndDeserialize<UserDTO>($"/users/{userName}");
+            var workAreaContexts = await _client.GetAndDeserialize<List<WorkAreaContextDTO>>($"/users/{user.Id}/workarea-contexts");
+
+            workAreaContexts.Count.Should().Be(2);
+            var workAreaContext = workAreaContexts.Find(x => x.TagName == EnumUtils.MapWorkAreaTypeToTagName(WorkAreaType.PR_TR));
+            workAreaContext.Should().NotBeNull();
+
+            var framework = await _client.GetAndDeserialize<FrameworkDTO>($"/frameworks/{workAreaContext.InstructionalFrameworkId}");
+            framework.Should().NotBeNull();
+            framework.Name.Should().Be("Danielson Instructional");
+
+            framework.FrameworkNodes.Count.Should().Be(8);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D1").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D2").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D3").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D4").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D5").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D6").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D7").Count().Should().Be(1);
+            framework.FrameworkNodes.Where(x => x.ShortName == "D8").Count().Should().Be(1);
+        }
+
+
+
     }
 }
