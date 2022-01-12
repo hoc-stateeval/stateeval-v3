25b12e3c670e6fed9d6cdcf9d31257310c7fb685
Anne Chinn <achinn@nwlink.com>
2022-01-11 21:07:40 -0800

    working on rubric navigator

--- src/frontend/src/components/RubricNavigator/RubricNavigator.js
+++ src/frontend/src/components/RubricNavigator/RubricNavigator.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
+import { useTheme } from "@mui/material/styles";
 import { styled } from "@mui/material/styles";
 import {
+  Box,
   Paper,
   Stack,
   Typography,
@@ -25,49 +27,82 @@ const rubricNavigatorNodeTextColor = 'white';
 const rubricNavigatorNodeHoverBackgroundColor ='#7FB8EC';
 const rubricNavigatorNodeHoverTextColor = '#2F4050';
 
-const RubricRowStyled = styled('div')(({ theme }) => ({
-  display: 'grid',
-  gridTemplateColumns:'3rem auto',
-  gridTemplateRows: '3rem',
-  "> div": {
-    height: '3rem',
-    lineHeight: '3rem',
-    borderLeft: `1px solid ${rubricNavigatorBorderColor}`,
-    borderTop: `1px solid ${rubricNavigatorBorderColor}`,
-    cursor: 'pointer',
-  },
-  "> div:first-of-type": {
-    textAlign: 'center',
-    width: '3rem',
-  },
-  ">div:nth-of-type(2)": {
-     borderRight: '1px solid $rubric-navigator-border-color',
-     padding: '3px',
-     textAlign: 'left',
-     width: 'auto',
-     //fontSize: '.75rem',
+export const getRowStyles = (theme, fn) => {
+
+  const buildFirstChildStyles = (fn) => {
+    if (fn) {
+      return {
+        ">div:first-of-type": {
+          textAlign: 'center',
+          width: '3rem',
+          backgroundColor: `${rubricNavigatorNodeBackgroundColor}`,
+          color: `${rubricNavigatorNodeTextColor}`,
+          fontWeight: '700',
+          "&:hover": {
+             backgroundColor: `${rubricNavigatorNodeHoverBackgroundColor}`,
+             color: `${rubricNavigatorNodeHoverTextColor}`,
+          }    
+        },
+      }
+    }
+    else {
+      return {
+        ">div:first-of-type": {
+          textAlign: 'center',
+          width: '3rem',
+         },
+      }
+    }
   }
-}));
+
+  let styles = {
+    display: 'grid',
+    gridTemplateColumns:'3rem auto',
+    gridTemplateRows: '3rem',
+    ">div": {
+      height: '3rem',
+      lineHeight: '3rem',
+      borderLeft: `1px solid ${rubricNavigatorBorderColor}`,
+      borderTop: `1px solid ${rubricNavigatorBorderColor}`,
+      cursor: 'pointer',
+    },
+    ">div:nth-of-type(2)": {
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      borderRight: `1px solid ${rubricNavigatorBorderColor}`,
+      padding: '3px',
+      textAlign: 'left',
+      width: 'auto',
+    },
+  };
+
+  return {
+    ...styles,
+    ...buildFirstChildStyles(fn)
+  }
+}
 
 const RubricNavigatorRubricRow = ({rubricRow}) => {
-
   const dispatch = useDispatch();
+  const theme = useTheme();
 
   const onClickRubricRow = () => {
     dispatch(setActiveRubricRowId(rubricRow.id));
   }
 
   return (
-    <RubricRowStyled>
+    <Box sx={{ ...getRowStyles(theme, false) }} >
         <div className="rr" onClick={()=>onClickRubricRow()}>{rubricRow?.shortName} </div>
         <div>{rubricRow.title}</div>
-    </RubricRowStyled>
+    </Box>
   )
 };
 
 const RubricNavigatorFrameworkNode = ({frameworkNode}) => {
 
   const dispatch = useDispatch();
+  const theme = useTheme();
 
   // const [ expanded, setExpanded ] = useState(false);
 
@@ -77,10 +112,10 @@ const RubricNavigatorFrameworkNode = ({frameworkNode}) => {
 
   return (
     <>
-      <div className="fn">
+      <Box sx={{ ...getRowStyles(theme, true) }} >
         <div onClick={()=>onClickFrameworkNode()}>{frameworkNode?.shortName} </div>
         <div>{frameworkNode.title}</div>
-      </div>
+      </Box>
 
       <div>
         {frameworkNode.rubricRows.map((x)=>(
@@ -97,7 +132,7 @@ const RubricNavigator = () => {
 
   return (
     <>
-      <Paper>
+      <Box sx={{borderBottom: `1px solid ${rubricNavigatorBorderColor}`}}>
         <Typography variant="h6">Rubric Navigator</Typography>
         <Stack spacing={3}>
           <div className="navigator">
@@ -108,7 +143,7 @@ const RubricNavigator = () => {
             </div>
           </div>
         </Stack>
-      </Paper>
+      </Box>
     </>
   );
 };
