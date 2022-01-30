bd12867ecd8225566a80817962acebc7f6085bf4
Anne Chinn <achinn@nwlink.com>
2022-01-29 21:06:05 -0800

    working on evidence collection

--- src/frontend/src/components/EvidenceCollection/RubricAlignmentSection.js
+++ src/frontend/src/components/EvidenceCollection/RubricAlignmentSection.js
@@ -1,24 +1,55 @@
-
+import { useSelector } from "react-redux";
 import { 
-  Box,
-  Divider,
-  IconButton,
   Paper,
   Stack,
-  Tooltip,
-  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@mui/material";
-import { useTheme } from '@mui/material/styles';
+
+import { 
+  selectActiveRubricRow,
+} from "@evidence-collection-slice";
 
 import { PageSectionHeader } from "@components";
 
 
+
 const RubricAlignmentSection = () => {
+
+  const activeRubricRow = useSelector(selectActiveRubricRow);
+
   return (
     <>
       <Stack spacing={2}>
         <PageSectionHeader title={"Rubric Alignment"}></PageSectionHeader>
+
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>UNS</TableCell>
+                <TableCell>BAS</TableCell>
+                <TableCell>PRO</TableCell>
+                <TableCell>DIS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow style={{verticalAlign:'top'}}>
+                <TableCell  dangerouslySetInnerHTML={{__html: `${activeRubricRow.pL1Descriptor}`}}></TableCell>
+                <TableCell dangerouslySetInnerHTML={{__html: `${activeRubricRow.pL2Descriptor}`}}></TableCell>
+                <TableCell dangerouslySetInnerHTML={{__html: `${activeRubricRow.pL3Descriptor}`}}></TableCell>
+                <TableCell dangerouslySetInnerHTML={{__html: `${activeRubricRow.pL4Descriptor}`}}></TableCell>
+              </TableRow>
+
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Stack>
+
     </>
   );
 };
