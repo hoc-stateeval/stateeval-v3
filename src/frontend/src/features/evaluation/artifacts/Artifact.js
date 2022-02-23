42e363f422505fb9ec255d8bd1f0f3b862b67417
Anne Chinn <achinn@nwlink.com>
2022-02-23 11:06:26 -0800

    added directories that weren't being found

---
+++ src/frontend/src/features/evaluation/artifacts/Artifact.js
@@ -0,0 +1,13 @@
+import {
+  Typography,
+} from '@mui/material';
+
+const Artifact = () => {
+  return (
+    <>
+    <Typography variant="h4">Artifact</Typography>
+    </>
+  );
+};
+
+export default Artifact;
---
+++ src/frontend/src/features/evaluation/artifacts/Dashboard.js
@@ -0,0 +1,13 @@
+import {
+  Typography,
+} from '@mui/material';
+
+const Dashboard = () => {
+  return (
+    <>
+    <Typography variant="h2">Artifacts</Typography>
+    </>
+  );
+};
+
+export default Dashboard;
