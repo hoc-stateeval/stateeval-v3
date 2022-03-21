f6a8907f54c09ac60930aa04162637e1c155425c
Anne Chinn <achinn@nwlink.com>
2022-03-21 00:07:34 -0700

    checking in

--- src/frontend/src/features/evaluation/perception-surveys/PerceptionSurvey.js
+++ src/frontend/src/features/evaluation/perception-surveys/PerceptionSurvey.js
@@ -1,12 +1,51 @@
 
+import {useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
   Typography
 } from '@mui/material';
 
+import {
+  useDeletePerceptionSurveyMutation
+} from "@api-slice";
+
 const PerceptionSurvey = () => {
+
+  const { id: surveyId } = useParams();
+
+  const [deleteSurveyDlgOpen, setDeleteSurveyDlgOpen] = useState(false);
+
+  const deleteSurvey = () => {
+
+  }
+
   return (
     <>
-      <Typography variant="h2">Perception Survey</Typography>
+      <Stack direction="column" spacing={2}>
+        <Stack direction="row" spacing={2} sx={{justifyContent:'flex-end'}}>
+          <Button onClick={()=> {setDeleteSurveyDlgOpen(true)}}>Delete Survey</Button>
+          <Dialog open={deleteSurveyDlgOpen} onClose={()=> {setDeleteSurveyDlgOpen(false)}}>
+            <DialogTitle>Delete</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Are you sure you want to delete this survey?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button variant="outlined"  onClick={()=> {setDeleteSurveyDlgOpen(false)}}>Cancel</Button>
+              <Button variant="contained" onClick={deleteSurvey}>Delete Survey</Button>
+            </DialogActions>
+          </Dialog>
+        </Stack>
+      </Stack>
     </>
   )
 }
